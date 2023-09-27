20798fcb276589ca9b6f5eef30c21cbb1831bcb8
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-09-27 20:08:11 +0300

    Добавил поле is_active_sale для модели Product

--- products/admin.py
+++ products/admin.py
@@ -4,5 +4,5 @@ from products.models import Product
 
 @admin.register(Product)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'shop_name', 'product_title', 'seller', 'price', )
+    list_display = ('id', 'shop_name', 'product_title', 'seller', 'price', 'is_active_sale', )
     list_display_links = ('product_title', )
